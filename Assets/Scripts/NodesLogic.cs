4e46010a4e1e72d45bac5775fa1f2e1b27b5418c
NevskyOne <wvwvalli@gmail.com>
2023-11-26 15:04:32 +0300

    add error catching

--- Assets/Scripts/NodesLogic.cs
+++ Assets/Scripts/NodesLogic.cs
@@ -1,16 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 public class NodesLogic : MonoBehaviour
 {
     [HideInInspector] public List<List<string>> Programm = new List<List<string>>(); // список позиций нодов для конечной программы
     List<List<List<string>>> AllCombinatoins = new List<List<List<string>>>(); // список позиций всех нодов по группам
     
-    [SerializeField] string InputText;
-    [SerializeField] string InputValue;
-    [SerializeField] string Answer;
-    [SerializeField] TMP_Text InputField;
-    [SerializeField] TMP_Text OutputField;
+    [SerializeField]
+    string InputText, InputValue, Answer;//переменная для текста ввода, значиния ввода и правильного ответа
+    [SerializeField]
+    TMP_Text InputField, OutputField;//переменная для поля ввода и вывода
+    [SerializeField]
+    GameObject Error;//изображение ошибки
     private void OnEnable() {
         InputField.text = InputText;
     }
@@ -93,7 +95,7 @@ public class NodesLogic : MonoBehaviour
              _strToInsert = -1;
              _progToInsert = -1;
         }
-        if(_addNew) AllCombinatoins.Add(new List<List<string>>(){new List<string>(){Obj.name}});//если нужно добавить
+        if(_addNew) AllCombinatoins.Add(new List<List<string>>(){new List<string>(){Obj.name}});//если нужно добавить новую группу с нодом
 
         foreach(int r in _progToRemove) AllCombinatoins.RemoveAt(r);//удаляем все группы из списка
         
@@ -101,37 +103,59 @@ public class NodesLogic : MonoBehaviour
     }
 
     public void StartProgramm(){
+        Error.SetActive(false);//деалем невидимой ошибку
+        OutputField.text = "";//обнуляем строку вывода
+        List<VariableNode> Variables = new();//создаем список всех переменных
 
-        List<VariableNode> Variables = new();
-
-        foreach(List<List<string>> i in AllCombinatoins){
-            if(i[0][0].StartsWith("BeginNode") && i[^1][0].StartsWith("EndNode")){
-                foreach(List<string> str in i){
+        foreach(List<List<string>> i in AllCombinatoins){//перебираем все группы нодов
+            if(i[0][0].StartsWith("BeginNode") && i[^1][0].StartsWith("EndNode")){//если есть начало и конец
+                foreach(List<string> str in i){//перебираем все строки
                     Debug.Log("["+string.Join(", ", str)+"]");
 
-                    if(str[0].StartsWith("VariableNode")){
-                        GameObject obj = GameObject.Find(str[0]);
-                        string _name = obj.transform.GetChild(1).GetComponent<TMP_InputField>().text;
-                        string _type;
-                        string _value = Assignment(AllCombinatoins.IndexOf(i),i.IndexOf(str));
-                        if(float.TryParse(_value,out var number)) _type = "float";
-                        else _type = "string";
-                        Variables.Add(new VariableNode(_name,_value, _type));
+                    GameObject obj = GameObject.Find(str[0]);//получаем объект по имени первого нода
+
+                    if(str[0].StartsWith("VariableNode")){//если объявляем переменную
+                        string _name = obj.transform.GetChild(1).GetComponent<TMP_InputField>().text;//имя
+                        if(_name == ""){//проверяет является ли поле пустым и выводит ошибку
+                            OutputField.text = "Ошибка, "+(i.IndexOf(str)+1)+" строка: отсутстувует имя переменной";
+                            Error.SetActive(true);
+                            return;
+                        }
+                        string _type;//тип
+                        string _value = Assignment(AllCombinatoins.IndexOf(i),i.IndexOf(str));//функция для определения значение
+                        if(_value.StartsWith("Ошибка")){//если ошибка, то делаем активной ошибку и вводим сообщение в вывод
+                            OutputField.text = _value;
+                            Error.SetActive(true);
+                            return;
+                        }
+                        if(float.TryParse(_value,out var number)) _type = "float";//если возможно перевести в float
+                        else _type = "string";//иначе оставляем строкой
+                        Variables.Add(new VariableNode(_name,_value, _type));//добавляем переменную
                         Debug.Log(_name+" "+_value+" "+ _type);
                     }
-                    else if(str[0].StartsWith("InputNode")){
-                        GameObject obj = GameObject.Find(str[0]);
-                        string _name = obj.transform.GetChild(1).GetComponent<TMP_InputField>().text;
-                        string _type;
-                        string _value = InputValue;
-                        if(float.TryParse(_value,out var number)) _type = "float";
-                        else _type = "string";
-                        Variables.Add(new VariableNode(_name,_value, _type));
+                    else if(str[0].StartsWith("InputNode")){//если нод ввода
+                        string _name = obj.transform.GetChild(1).GetComponent<TMP_InputField>().text;//имя переменной
+                        if(_name == ""){//проверяет является ли поле пустым и выводит ошибку
+                            OutputField.text = "Ошибка, "+(i.IndexOf(str)+1)+" строка: отсутстувует переменная ввода";
+                            Error.SetActive(true);
+                            return;
+                        }
+                        string _type;//тип
+                        string _value = InputValue;//заносим значение из испектора
+                        if(float.TryParse(_value,out var number)) _type = "float";//если возможно перевести в float
+                        else _type = "string";//иначе оставляем строкой
+                        Variables.Add(new VariableNode(_name,_value, _type));//добавляем переменную
                     }
-                    else if(str[0].StartsWith("OutputNode")){
-                        GameObject obj = GameObject.Find(str[0]);
-                        OutputField.text = Variables.Find(x => x.Name == obj.transform.GetChild(1).GetComponent<TMP_InputField>().text).Value;
-                        if(Variables.Find(x => x.Name == obj.transform.GetChild(1).GetComponent<TMP_InputField>().text).Value == Answer){
+                    else if(str[0].StartsWith("OutputNode")){//если нод вывода
+                        try{
+                            OutputField.text = Variables.Find(x => x.Name == obj.transform.GetChild(1).GetComponent<TMP_InputField>().text).Value;//находим значение переменной и выносим в поле вывода
+                        }
+                        catch(NullReferenceException){//проверяет является ли поле пустым и выводит ошибку
+                            OutputField.text = "Ошибка, "+(i.IndexOf(str)+1)+" строка: отсутстувует переменная вывода";
+                            Error.SetActive(true);
+                            return;
+                        }
+                        if(Variables.Find(x => x.Name == obj.transform.GetChild(1).GetComponent<TMP_InputField>().text).Value == Answer){//проверяем правильность ответа
                             GameObject.Find("Correct").GetComponent<Animation>().Play();
                         }
                         else GameObject.Find("Incorrect").GetComponent<Animation>().Play();
@@ -155,7 +179,7 @@ public class NodesLogic : MonoBehaviour
                         string _txt = GameObject.Find(node).transform.GetChild(1).GetComponent<TMP_InputField>().text;
                         _value += Variables.Find(x => x.Name ==  _txt).Value;
                     }
-                    else if(GameObject.Find(node).tag=="MathNodes" && _currentStr.Count>=_nodeCount+1 && (_currentStr[_nodeCount-1].StartsWith("StringNode") || _currentStr[_nodeCount-1].StartsWith("NumberNode") || _currentStr[_nodeCount-1].StartsWith("VariableNode")) && (_currentStr[_nodeCount+1].StartsWith("StringNode") || _currentStr[_nodeCount+1].StartsWith("NumberNode") || _currentStr[_nodeCount+1].StartsWith("VariableNode"))){
+                    else if(GameObject.Find(node).tag=="MathNodes" && _currentStr.Count>_nodeCount+1 && (_currentStr[_nodeCount-1].StartsWith("StringNode") || _currentStr[_nodeCount-1].StartsWith("NumberNode") || _currentStr[_nodeCount-1].StartsWith("VariableNode")) && (_currentStr[_nodeCount+1].StartsWith("StringNode") || _currentStr[_nodeCount+1].StartsWith("NumberNode") || _currentStr[_nodeCount+1].StartsWith("VariableNode"))){
 
                         string _value2 = "";
                         string _lastTxt = GameObject.Find(_currentStr[_nodeCount-1]).transform.GetChild(1).GetComponent<TMP_InputField>().text;
@@ -173,7 +197,7 @@ public class NodesLogic : MonoBehaviour
                         else if((_currentStr[_nodeCount-1].StartsWith("StringNode") || Variables.Exists(x => x.Name == _lastTxt && x.Type == "string")) && Variables.Exists(x => x.Name == _nextTxt && x.Type == "string")) {
                             _value2 = Variables.Find(x => x.Name == _nextTxt).Value;
                         }
-                        else return "";
+                        else return "Ошибка, "+(str+1)+" строка: неверные типы данных";
 
                         if(float.TryParse(_value,out var number1) && float.TryParse(_value2,out var number2)){
                             if(node.StartsWith("AdditionNode")) _value=(float.Parse(_value) + float.Parse(_value2)).ToString();
@@ -187,14 +211,22 @@ public class NodesLogic : MonoBehaviour
                         }
                         else{
                             if(node.StartsWith("AdditionNode")) _value+=_value2;
-                            else return "";
+                            else return "Ошибка, "+(str+1)+" строка: неверные типы данных";
                         }
                     }
                     
+                    else if(!node.StartsWith("NumberNode") && !node.StartsWith("StringNode") && !node.StartsWith("VariableNode") && !node.StartsWith("AssignmentNode")){
+                        return "Ошибка, "+(str+1)+" строка: ошибка при объявлении переменной";
+                    }
+                    else if((node.StartsWith("NumberNode") || node.StartsWith("StringNode") || node.StartsWith("VariableNode")) && _nodeCount>1 && !_currentStr[_nodeCount-1].StartsWith("AssignmentNode") &&(GameObject.Find(_currentStr[_nodeCount-1]).tag!="MathNodes")){
+                        return "Ошибка, "+(str+1)+" строка: ошибка при объявлении переменной";
+                    }
                     _nodeCount++;
                 }
+                return _value;
             }
-            return _value;
+            else return "Ошибка, "+(str+1)+" строка: отсутствует нод присваивания";
+            
         }
     }
 }

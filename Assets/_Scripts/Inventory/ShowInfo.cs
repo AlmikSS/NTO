3b26e286916638f6d16ee5e0f064eecf885f5e0e
AlmikSS <ulmasoviskandar7@gmail.com>
2023-12-10 09:15:58 +0300

    Продолжаю работать над показом информации в инвентаре

---
+++ Assets/_Scripts/Inventory/ShowInfo.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ShowInfo : MonoBehaviour
+{
+    [SerializeField] TMP_Text _text;
+
+    public void Show(string description)
+    {
+        _text.text = description;
+    }
+}
